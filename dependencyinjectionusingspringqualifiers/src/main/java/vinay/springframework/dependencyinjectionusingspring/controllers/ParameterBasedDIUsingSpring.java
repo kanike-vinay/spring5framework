5d32b3eb1543d1e9f6180a4e886fa0671ddb4b5e
kanike-vinay <kanike.vinay@gmail.com>
2019-05-11 14:45:33 +0530

    parameter based DI, handling in multiple ways

--- dependencyinjectionusingspringqualifiers/src/main/java/vinay/springframework/dependencyinjectionusingspring/controllers/ParameterBasedDIUsingSpring.java
+++ dependencyinjectionusingspringqualifiers/src/main/java/vinay/springframework/dependencyinjectionusingspring/controllers/ParameterBasedDIUsingSpring.java
@@ -1,17 +1,19 @@
 package vinay.springframework.dependencyinjectionusingspring.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import vinay.springframework.dependencyinjectionusingspring.services.GreetingService;
 
 @Controller
 public class ParameterBasedDIUsingSpring {
     @Autowired
-    @Qualifier("greetingServiceImpl")
-    public GreetingService greetingService;
+    //@Qualifier("greetingServiceImpl") //method 1
+    //public GreetingService greetingService; // method 1
+
+    //public GreetingServiceImpl greetingService; // method 2 , looks in the type and inject
+    public GreetingService greetingServiceImpl; // method 3 , looks in the name and inject (basically reflection is happening.
 
     public String hello(){
-        return greetingService.sayHello();
+        return greetingServiceImpl.sayHello();
     }
 }
